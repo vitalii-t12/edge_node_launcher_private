0c08cbc30f2097f12cea6925e9f6110729f73d36
Cristi Bleotiu <cristibleotiu@gmail.com>
2024-09-27 12:05:37 +0300

    fix: explicitly set the working directory

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import sys
+import os
 from PyQt5.QtWidgets import QApplication
 from app_forms.frm_main import EdgeNodeLauncher
-                             
+
+# Set the working directory to the location of the executable
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 if __name__ == '__main__':
   app = QApplication(sys.argv)
   manager = EdgeNodeLauncher()
