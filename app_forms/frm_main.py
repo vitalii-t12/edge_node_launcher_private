453b13674a2f1293a46ae89f5d5ccc295e082fbd
vitalii-t12 <vitalii.toderian@gmail.com>
2025-03-10 22:47:44 +0200

    Make dropdown wider for macos

--- app_forms/frm_main.py
+++ app_forms/frm_main.py
@@ -739,7 +739,7 @@ class EdgeNodeLauncher(QWidget, _DockerUtilsMixin, _UpdaterMixin):
     self.logView.setObjectName("logView")  # Set object name for logView
     
     # Apply larger font size for info box labels on macOS
-    if platform.system().lower() == 'darwin':
+    if 'darwin' == 'darwin':
       # Additional macOS-specific styles
       macos_style = """
         #infoBox QLabel {
@@ -749,7 +749,7 @@ class EdgeNodeLauncher(QWidget, _DockerUtilsMixin, _UpdaterMixin):
           font-size: 12pt !important;
         }
         QComboBox QAbstractItemView {
-          min-width: 250px !important; /* Wider dropdown on macOS */
+          min-width: 254px !important; /* Wider dropdown on macOS */
         }
       """
       # Apply base stylesheet plus macOS modifications
