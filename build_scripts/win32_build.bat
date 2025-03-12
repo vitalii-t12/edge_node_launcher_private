bd20d13efe9076d45430bfefe61576e30fb32713
vitalii-t12 <vitalii.toderian@gmail.com>
2025-03-12 14:11:51 +0200

    Use icons for build images

--- build_scripts/win32_build.bat
+++ build_scripts/win32_build.bat
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 
 REM Your base PyInstaller command
-set PYINSTALLER_CMD=pyinstaller -w --onefile -n "EdgeNodeLauncher" main.py
+set PYINSTALLER_CMD=pyinstaller -w --onefile -n "EdgeNodeLauncher" --icon=assets\r1_icon.ico main.py
 
 REM Combine the base command with the hidden imports and execute
 echo %PYINSTALLER_CMD%
