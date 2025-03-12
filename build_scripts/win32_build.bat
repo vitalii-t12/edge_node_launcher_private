510861693e108cfd824d0b1da801f6e2a7d15a26
vitalii-t12 <vitalii.toderian@gmail.com>
2025-03-12 13:39:15 +0200

    Revert "Add msi executable for windows"
    This reverts commit 1193b7aaa9f762cbc9c0d630a06984a98c3df192.

--- build_scripts/win32_build.bat
+++ build_scripts/win32_build.bat
@@ -1,10 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-REM Check for parameters
-set CREATE_MSI=0
-if "%1"=="--msi" set CREATE_MSI=1
-
 REM Your base PyInstaller command
 set PYINSTALLER_CMD=pyinstaller -w --onefile -n "EdgeNodeLauncher" main.py
 
@@ -12,15 +8,4 @@ REM Combine the base command with the hidden imports and execute
 echo %PYINSTALLER_CMD%
 %PYINSTALLER_CMD%
 
-REM Optionally create MSI installer
-if %CREATE_MSI% EQU 1 (
-    echo Creating MSI installer...
-    call create_msi.bat
-    if %ERRORLEVEL% NEQ 0 (
-        echo MSI creation failed.
-        exit /b 1
-    )
-)
-
-echo Build process completed successfully!
 endlocal
